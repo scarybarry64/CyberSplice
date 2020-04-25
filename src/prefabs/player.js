4b074b1fdf6703b16c1d20168d49c8890a6d5d1f
tmoropou <tmoropou@ucsc.edu>
2020-04-25 04:30:31 -0700

    Removed player prefab physics settings

--- src/prefabs/player.js
+++ src/prefabs/player.js
@@ -5,6 +5,15 @@ class Player extends Phaser.Physics.Arcade.Sprite {
 
         // adding the object to the existing scene so it shows up
         scene.add.existing(this);
+        // enable its physics
+        scene.physics.add.existing(this);
+
+        // amount of bounciness
+        this.setBounce(0.1, 0.1);
+
+        //currently using this until floors added
+        this.setCollideWorldBounds(true); 
+        this.body.onWorldBounds = true;
     }
     
 }
