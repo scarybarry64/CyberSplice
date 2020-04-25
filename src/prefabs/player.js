71af761ab1f19010e0e0c5b555aad6144029c552
tmoropou <tmoropou@ucsc.edu>
2020-04-24 21:24:30 -0700

    Created Player prefab

--- src/prefabs/player.js
+++ src/prefabs/player.js
@@ -1,4 +1,10 @@
 // Player prefab
-class Player extends Phaser.GameObjects.Sprite {
-    
+class Player extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+
+        // adding the object to the existing scene so it shows up
+        scene.add.existing(this);
+    }
+
 }
