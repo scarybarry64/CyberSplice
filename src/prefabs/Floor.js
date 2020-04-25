80523143b8a0ab8a1a1579d132d5fd4fb3d6c62d
tmoropou <tmoropou@ucsc.edu>
2020-04-25 04:30:05 -0700

    removed static aspect of floor prefab

--- src/prefabs/Floor.js
+++ src/prefabs/Floor.js
@@ -5,5 +5,8 @@ class Floor extends Phaser.Physics.Arcade.Sprite {
 
         // adding the object to the existing scene so it shows up
         scene.add.existing(this);
+        // enable its physics, static so it doesnt fall with gravity
+        scene.physics.add.staticGroup(this);
+
     }
 }
