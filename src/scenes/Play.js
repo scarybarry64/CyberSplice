2d20113510ee1c783f3d6d85e03345d28aefdb0f
tmoropou <tmoropou@ucsc.edu>
2020-04-24 21:55:28 -0700

    Added floor to play scene

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,6 +6,8 @@ class Play extends Phaser.Scene {
     preload() {
         // load the necessary images and tile sprites
         this.load.image('pixel_guy', './assets/sprites/pixel_guy.png'); //placeholder
+        this.load.image('bounds', './assets/sprites/bounds.png'); //placeholder
+    
     }
 
     create() {
@@ -13,5 +15,9 @@ class Play extends Phaser.Scene {
         // spawn player
         this.player1 = new Player(this, game.config.width/2, 255,
             'pixel_guy').setScale(1, 1).setOrigin(0,0);
+
+        //spawn floor
+        this.floor1 = new Floor(this, -25, 550,
+            'bounds').setScale(4,0.5).setOrigin(0,0);
     }
 }
